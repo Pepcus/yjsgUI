e593379441bd5e7f03ca2e090023c4f8e08a4cdc
Bhagyashree Bhoyar <bhagyashree.bhoyar@pepcus.com>
2018-12-24 12:58:59 +0530

    Resole back button redirect.

--- src/components/coreComponents/Routes.jsx
+++ src/components/coreComponents/Routes.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import SplashPage from '../SplashPage';
 import StudentRegistrationForm from '../StudentRegistrationForm';
