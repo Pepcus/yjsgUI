b1aedfacbc8eb021b8316649d922494f3994c93f
Priyal Jain <priyal.jain@pepus.com>
2019-04-01 16:33:03 +0530

    new-styling

--- src/utils/labelConstants.jsx
+++ src/utils/labelConstants.jsx
@@ -33,7 +33,7 @@ export const MARK_SELECTED_STUDENTS_ATTENDANCE_LABEL = 'कृपिया च�
 
 export const OPT_IN_STATUS_FOR_SELECTED_STUDENTS_LABEL = 'कृपया चयनित छात्रों की शिविर के लिए हाँ या ना दर्ज़ करें';
 
-export const ID_CARD_PRINT_STATUS_FOR_SELECTED_STUDENTS_LABEL = 'कृपया दिए गए IDs को  कार्ड प्रिंट के लिए चयन करें!';
+export const ID_CARD_PRINT_STATUS_FOR_SELECTED_STUDENTS_LABEL = 'कृपया दिए गए ID\'s को  कार्ड प्रिंट के लिए चयन करें!';
 
 export const BUS_NUMBER_LABEL = 'Bus#';
 
