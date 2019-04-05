82950d5ce4ade33886875bdfc68fb81ca8697a71
Bhagyashree Bhoyar <bhagyashree.bhoyar@pepcus.com>
2019-04-05 16:26:03 +0530

    Changed grid version and added support for empty cell representation according to user choice.

--- src/utils/dataGridUtils.js
+++ src/utils/dataGridUtils.js
@@ -97,7 +97,6 @@ export const chunkArray = (Array, chunkSize) => {
 };
 
 export const setAppColor = (mode) => {
-  console.log('mode --- ', mode);
   for (const key in mode) {
     if (mode.hasOwnProperty(key)) {
       document.documentElement.style.setProperty(key, mode[key]);
