c7dc12112fdfb4a41c2673f7398afb5b6f47f942
Bhagyashree Bhoyar <bhagyashree.bhoyar@pepcus.com>
2019-04-23 15:21:39 +0530

    Make route by json file configure.

--- src/components/coreComponents/Routes.jsx
+++ src/components/coreComponents/Routes.jsx
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import uniqueId from 'lodash/uniqueId';
 
-import SplashPage from '../SplashPage';
-import StudentRegistrationForm from '../StudentRegistrationForm';
-import StudentRegistrationCorrectionForm from '../StudentRegistrationCorrectionForm';
 import Footer from './Footer';
 import Loader from '../Loader';
-import SplashPagePrePopulated from '../SplashPagePrePopulated';
-import AdminPanel from '../AdminPanel';
-import StudentInformationGrid from '../StudentInformationGrid';
-import StudentCredentialPage from '../StudentCredentialPage';
-import StudentPage from '../StudentPage';
 import Context from './ConfigProvider';
-import ParentsRegistration from '../ParentsRegistrationForm';
-import Files from '../Files';
+import components from '../routeComponents/index';
 import Header from '../Header';
+import { RouteArray } from '../../config/appConfig.json';
+
 
 /**
  * Routes component maintain all routes
@@ -37,7 +31,23 @@ class Routes extends Component {
       });
     }
   }
-
+  getRouts = Consumer => RouteArray.map((routeObject) => {
+    const ComponentName = components[routeObject.componentName];
+    if (routeObject.isActive) {
+      return (
+        <Route
+          key={uniqueId(routeObject.componentName)}
+          exact
+          path={routeObject.routePath}
+          component={() => (
+            <Consumer>
+              { context => <ComponentName context={context} /> }
+            </Consumer>
+            )}
+        />
+      );
+    } return null;
+  });
   render() {
     const { Consumer } = Context;
     return (
@@ -46,96 +56,7 @@ class Routes extends Component {
           <Consumer>
             {context => <Header context={context} location={this.props.location.pathname} />}
           </Consumer>
-          <Route
-            exact
-            path="/"
-            component={() => (
-              <Consumer>
-                {context => <StudentPage context={context} />}
-              </Consumer>
-            )}
-          />
-          <Route
-            exact
-            path="/admin"
-            component={() => (
-              <Consumer>
-                {context => <SplashPage context={context} />}
-              </Consumer>
-            )}
-          />
-          <Route
-            exact
-            path="/student-search"
-            component={() => (
-              <Consumer>
-                {context => <StudentInformationGrid context={context} />}
-              </Consumer>
-            )}
-          />
-          <Route
-            exact
-            path="/home"
-            component={() => (
-              <Consumer>
-                {context => <SplashPagePrePopulated context={context} />}
-              </Consumer>
-            )}
-          />
-          <Route
-            exact
-            path="/studentRegister"
-            component={() => (
-              <Consumer>
-                {context => <StudentRegistrationForm context={context} />}
-              </Consumer>
-            )}
-          />
-          <Route
-            exact
-            path="/studentCorrection"
-            component={() => (
-              <Consumer>
-                {context => <StudentRegistrationCorrectionForm context={context} />}
-              </Consumer>
-            )}
-          />
-          <Route
-            exact
-            path="/adminPanel"
-            component={() => (
-              <Consumer>
-                {context => <AdminPanel context={context} />}
-              </Consumer>
-            )}
-          />
-          <Route
-            exact
-            path="/student-login"
-            component={() => (
-              <Consumer>
-                {context => <StudentCredentialPage context={context} />}
-              </Consumer>
-            )}
-          />
-          <Route
-            exact
-            path="/event"
-            component={() => (
-              <Consumer>
-                {context => <ParentsRegistration context={context} />}
-              </Consumer>
-            )}
-          />
-          <Route
-            exact
-            path="/files"
-            component={() => (
-              <Consumer>
-                {context => <Files context={context} />}
-              </Consumer>
-            )}
-          />
+          {this.getRouts(Consumer)}
           <Loader />
           <Footer />
         </Context.Provider>
---
+++ src/components/routeComponents/index.js
@@ -0,0 +1,23 @@
+import SplashPage from '../SplashPage';
+import StudentRegistrationForm from '../StudentRegistrationForm';
+import StudentRegistrationCorrectionForm from '../StudentRegistrationCorrectionForm';
+import SplashPagePrePopulated from '../SplashPagePrePopulated';
+import AdminPanel from '../AdminPanel';
+import StudentInformationGrid from '../StudentInformationGrid';
+import StudentCredentialPage from '../StudentCredentialPage';
+import StudentPage from '../StudentPage';
+import ParentsRegistration from '../ParentsRegistrationForm';
+import Files from '../Files';
+
+export default {
+  StudentPage,
+  SplashPage,
+  StudentRegistrationForm,
+  StudentRegistrationCorrectionForm,
+  SplashPagePrePopulated,
+  AdminPanel,
+  StudentInformationGrid,
+  StudentCredentialPage,
+  ParentsRegistration,
+  Files,
+};
