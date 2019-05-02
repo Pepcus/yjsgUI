25a143caee033dc6303b9a088e5c18e19abf2443
Bhagyashree Bhoyar <bhagyashree.bhoyar@pepcus.com>
2019-05-02 16:33:43 +0530

    Make text constants from json config.

--- src/constants/text.js
+++ src/constants/text.js
@@ -1,31 +1,33 @@
-export const PLEASE_SELECT_ANY_ONE_TEXT = 'कृपया एक चुनिए . . .';
+import { text } from '../config/constantsConfig.json';
 
-export const YES_TEXT = 'हाँ';
+export const PLEASE_SELECT_ANY_ONE_TEXT = text.PLEASE_SELECT_ANY_ONE_TEXT ? text.PLEASE_SELECT_ANY_ONE_TEXT : 'कृपया एक चुनिए . . .';
 
-export const NO_TEXT = 'ना';
+export const YES_TEXT = text.YES_TEXT ? text.YES_TEXT : 'हाँ';
 
-export const ID_NUMBER_TEXT = 'आई.डी. नं.';
+export const NO_TEXT = text.NO_TEXT ? text.NO_TEXT : 'ना';
 
-export const SECRET_CODE_TEXT = 'सीक्रेट कोड';
+export const ID_NUMBER_TEXT = text.ID_NUMBER_TEXT ? text.ID_NUMBER_TEXT : 'आई.डी. नं.';
 
-export const UPDATE_FURTHER_INFORMATION_TEXT = 'कृपिया अन्य जानकारी बदलने हेतु यहाँ';
+export const SECRET_CODE_TEXT = text.SECRET_CODE_TEXT ? text.SECRET_CODE_TEXT : 'सीक्रेट कोड';
 
-export const CLICK_HERE_TEXT = 'क्लिक करे |';
+export const UPDATE_FURTHER_INFORMATION_TEXT = text.UPDATE_FURTHER_INFORMATION_TEXT ? text.UPDATE_FURTHER_INFORMATION_TEXT : 'कृपिया अन्य जानकारी बदलने हेतु यहाँ';
 
-export const YOUR_ID_TEXT = 'आपका ID: ';
+export const CLICK_HERE_TEXT = text.CLICK_HERE_TEXT ? text.CLICK_HERE_TEXT : 'क्लिक करे |';
 
-export const YOUR_SECRET_CODE_TEXT = 'आपका सीक्रेट कोड: ';
+export const YOUR_ID_TEXT = text.YOUR_ID_TEXT ? text.YOUR_ID_TEXT : 'आपका ID: ';
 
-export const PLEASE_SELECT_COLUMNS_TEXT = 'कृपया स्तंभों का चयन करे';
+export const YOUR_SECRET_CODE_TEXT = text.YOUR_SECRET_CODE_TEXT ? text.YOUR_SECRET_CODE_TEXT : 'आपका सीक्रेट कोड: ';
 
-export const UPLOAD_FILE_TEXT = 'कृपिया फाइल को अपलोड करे';
+export const PLEASE_SELECT_COLUMNS_TEXT = text.PLEASE_SELECT_COLUMNS_TEXT ? text.PLEASE_SELECT_COLUMNS_TEXT : 'कृपया स्तंभों का चयन करे';
 
-export const IS_THERE_TEXT = ' है |';
+export const UPLOAD_FILE_TEXT = text.UPLOAD_FILE_TEXT ? text.UPLOAD_FILE_TEXT : 'कृपिया फाइल को अपलोड करे';
 
-export const ERROR_MESSAGE_OF_LOAD_APP_DATA = 'Error occurred to load application data.';
+export const IS_THERE_TEXT = text.IS_THERE_TEXT ? text.IS_THERE_TEXT : ' है |';
 
-export const FILE_DOWNLOAD_MESSAGE = 'File downloading started...';
+export const ERROR_MESSAGE_OF_LOAD_APP_DATA = text.ERROR_MESSAGE_OF_LOAD_APP_DATA ? text.ERROR_MESSAGE_OF_LOAD_APP_DATA : 'Error occurred to load application data.';
 
-export const YJSG_ID_CARD_SMALL_HEADING = 'जैन बाल एवं युवा संस्कार शिक्षन शिविर - 2019';
+export const FILE_DOWNLOAD_MESSAGE = text.FILE_DOWNLOAD_MESSAGE ? text.FILE_DOWNLOAD_MESSAGE : 'File downloading started...';
 
-export const YJSG_ID_CARD_MAIN_HEADING = 'Young Jain Study Group (YJSG)';
+export const YJSG_ID_CARD_SMALL_HEADING = text.YJSG_ID_CARD_SMALL_HEADING ? text.YJSG_ID_CARD_SMALL_HEADING : 'जैन बाल एवं युवा संस्कार शिक्षन शिविर - 2019';
+
+export const YJSG_ID_CARD_MAIN_HEADING = text.YJSG_ID_CARD_MAIN_HEADING ? text.YJSG_ID_CARD_MAIN_HEADING : 'Young Jain Study Group (YJSG)';
