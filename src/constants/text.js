e1e53e2a8f232c49b1d9194ac51b6eac2e240907
BhagyashreeBhoyar <45935228+BhagyashreeBhoyar@users.noreply.github.com>
2019-05-02 17:39:46 +0530

    Make text constants from json config. (#163)

--- src/constants/text.js
+++ src/constants/text.js
@@ -1,31 +1,33 @@
-export const PLEASE_SELECT_ANY_ONE_TEXT = 'कृपया एक चुनिए . . .';
+import { text } from '../config/constants.json';
 
-export const YES_TEXT = 'हाँ';
+export const PLEASE_SELECT_ANY_ONE_TEXT = text.PLEASE_SELECT_ANY_ONE_TEXT || '';
 
-export const NO_TEXT = 'ना';
+export const YES_TEXT = text.YES_TEXT || '';
 
-export const ID_NUMBER_TEXT = 'आई.डी. नं.';
+export const NO_TEXT = text.NO_TEXT || '';
 
-export const SECRET_CODE_TEXT = 'सीक्रेट कोड';
+export const ID_NUMBER_TEXT = text.ID_NUMBER_TEXT || '';
 
-export const UPDATE_FURTHER_INFORMATION_TEXT = 'कृपिया अन्य जानकारी बदलने हेतु यहाँ';
+export const SECRET_CODE_TEXT = text.SECRET_CODE_TEXT || '';
 
-export const CLICK_HERE_TEXT = 'क्लिक करे |';
+export const UPDATE_FURTHER_INFORMATION_TEXT = text.UPDATE_FURTHER_INFORMATION_TEXT || '';
 
-export const YOUR_ID_TEXT = 'आपका ID: ';
+export const CLICK_HERE_TEXT = text.CLICK_HERE_TEXT || '';
 
-export const YOUR_SECRET_CODE_TEXT = 'आपका सीक्रेट कोड: ';
+export const YOUR_ID_TEXT = text.YOUR_ID_TEXT || '';
 
-export const PLEASE_SELECT_COLUMNS_TEXT = 'कृपया स्तंभों का चयन करे';
+export const YOUR_SECRET_CODE_TEXT = text.YOUR_SECRET_CODE_TEXT || '';
 
-export const UPLOAD_FILE_TEXT = 'कृपिया फाइल को अपलोड करे';
+export const PLEASE_SELECT_COLUMNS_TEXT = text.PLEASE_SELECT_COLUMNS_TEXT || '';
 
-export const IS_THERE_TEXT = ' है |';
+export const UPLOAD_FILE_TEXT = text.UPLOAD_FILE_TEXT || '';
 
-export const ERROR_MESSAGE_OF_LOAD_APP_DATA = 'Error occurred to load application data.';
+export const IS_THERE_TEXT = text.IS_THERE_TEXT || '';
 
-export const FILE_DOWNLOAD_MESSAGE = 'File downloading started...';
+export const ERROR_MESSAGE_OF_LOAD_APP_DATA = text.ERROR_MESSAGE_OF_LOAD_APP_DATA || '';
 
-export const YJSG_ID_CARD_SMALL_HEADING = 'जैन बाल एवं युवा संस्कार शिक्षन शिविर - 2019';
+export const FILE_DOWNLOAD_MESSAGE = text.FILE_DOWNLOAD_MESSAGE || '';
 
-export const YJSG_ID_CARD_MAIN_HEADING = 'Young Jain Study Group (YJSG)';
+export const YJSG_ID_CARD_SMALL_HEADING = text.YJSG_ID_CARD_SMALL_HEADING || '';
+
+export const YJSG_ID_CARD_MAIN_HEADING = text.YJSG_ID_CARD_MAIN_HEADING || '';
