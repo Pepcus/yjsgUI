2c971b6159554732607a0e73f7ef693be61f5733
Bhagyashree Bhoyar <bhagyashree.bhoyar@pepcus.com>
2019-06-17 12:04:36 +0530

    eslint changes.

--- .eslintrc.js
+++ .eslintrc.js
@@ -311,7 +311,7 @@ module.exports = {
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-render-return-value.md
     'react/no-render-return-value': 'error',
 
-    // require a shouldComponeSampleFormValidation.jsntUpdate method, or PureRenderMixin
+    // require a shouldComponentUpdate method, or PureRenderMixin
     // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/require-optimization.md
     'react/require-optimization': ['off', { allowDecorators: [] }],
 
