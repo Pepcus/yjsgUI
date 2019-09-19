78ff5a9101d034ad67aa0b9154f9496294917b0e
Bhagyashree Bhoyar <bhagyashree.bhoyar@pepcus.com>
2019-09-19 16:11:45 +0530

    Files page with pepcus core.

--- src/components/routeComponents/index.js
+++ src/components/routeComponents/index.js
@@ -1,21 +1,17 @@
 import SplashPage from './splashPage';
 import MemberRegistrationForm from './memberRegistrationForm';
 import MemberRegistrationCorrectionForm from './memberRegistrationCorrectionForm';
-import SplashPagePrePopulated from './SplashPagePrePopulated';
-import AdminPanel from './AdminPanel';
 import MemberInformationGrid from './memberInformationGrid';
 import MemberCredentialPage from './memberCredentialPage';
+import Files from './files';
 import MemberPage from './memberPage';
 import ParentsRegistration from './ParentsRegistrationForm';
-import Files from './Files';
 
 export default {
   MemberPage,
   SplashPage,
   MemberRegistrationForm,
   MemberRegistrationCorrectionForm,
-  SplashPagePrePopulated,
-  AdminPanel,
   MemberInformationGrid,
   MemberCredentialPage,
   ParentsRegistration,
