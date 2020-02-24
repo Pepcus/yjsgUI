919aa471de0a205f233d3c8acf43fdf3eb00c33a
Amit jain <amit.jain@pepcus.com>
2020-02-24 16:26:40 +0530

    Fixed loader issues.

--- src/components/common/CustomLoader.jsx
+++ src/components/common/CustomLoader.jsx
@@ -19,6 +19,16 @@ const LoaderStyled = styled(Box)`
 
 const CustomLoader = () => {
   const customStyles = {
+    loaderWrapper: {
+      width: '100%',
+      height: '100%',
+      background: 'rgba(255, 255, 255, 0.8)',
+      zIndex: '100',
+      display: 'block',
+      position: 'fixed',
+      top: '0',
+      left: '0'
+    },
     loader: {
       position: 'absolute',
       top: '50%',
@@ -26,21 +36,23 @@ const CustomLoader = () => {
       margin: 0,
       display: 'inline',
       transform: 'translate(-50%, -50%)',
-      zIndex: 99999,
+      zIndex: 99,
     },
   };
   return (
-    <div style={customStyles.loader}>
-      <div className="lds-css">
-        <div className="lds-spinner">
-          {times(12,
-            () =>
-              (<LoaderStyled
-                padding="0"
-                borderStyle="none"
-                key={uniqueId('loaderSection')}
-               />))
-          }
+    <div style={customStyles.loaderWrapper}>
+      <div style={customStyles.loader}>
+        <div className="lds-css">
+          <div className="lds-spinner">
+            {times(12,
+              () =>
+                (<LoaderStyled
+                  padding="0"
+                  borderStyle="none"
+                  key={uniqueId('loaderSection')}
+                />))
+            }
+          </div>
         </div>
       </div>
     </div>
